e506038bdcc495b6af9883b3225bba5bf964c20e
Dan Looten <danwlooten@gmail.com>
2021-09-25 01:23:44 -0500

    Reorganized the cache file variables

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,3 @@
-const CACHE_NAME = "static-cache-v2";
-const DATA_CACHE_NAME = "data-cache-v1";
 const FILES_TO_CACHE = [
   "/",
   "/js/indexedDB.js",
@@ -10,6 +8,8 @@ const FILES_TO_CACHE = [
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png"
 ];
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
 
 // install
 self.addEventListener("install", function (evt) {
