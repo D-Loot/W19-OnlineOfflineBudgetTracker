e770a00ff803513b1b47cd766152b27b4d98d452
Dan Looten <danwlooten@gmail.com>
2021-09-24 22:20:40 -0500

    add indexedDB.js to cached file list, reorder list

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,10 +2,11 @@ const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 const FILES_TO_CACHE = [
   "/",
+  "/js/indexedDB.js",
   "/index.html",
   "/index.js",
   "/manifest.webmanifest",
-  "/style.css",
+  "/styles.css",
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png"
 ];
